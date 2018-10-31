36a1456bc262f3f38903a8de7c05863d86d0028d
wxsm <wxsms@foxmail.com>
2018-10-31 19:29:44 +0800

    jd web verify code auto fill

--- src/modules/jd/auth/web.js
+++ src/modules/jd/auth/web.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
 const path = require('path')
+const {createCanvas, Image} = require('canvas')
 const {abortUselessRequests} = require('../../../utils/puppeteer')
 const md5 = require('../../../utils/md5')
 const base64 = require('../../../utils/base64')
@@ -10,9 +11,37 @@ function getCookiePathByUser (user) {
   return path.join(__dirname, '../../../../temp/', md5('cookies-jd' + user.username))
 }
 
+function getVerifyPosition (base64, actualWidth) {
+  return new Promise((resolve, reject) => {
+    const canvas = createCanvas(1000, 1000)
+    const ctx = canvas.getContext('2d')
+    const img = new Image()
+    img.onload = () => {
+      const width = img.naturalWidth
+      const height = img.naturalHeight
+      ctx.drawImage(img, 0, 0)
+      let cloestX = 0
+      let minSum = 255 * 3
+      for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+          const data = ctx.getImageData(i, j, 1, 1).data
+          const rgbSum = data[0] + data[1] + data[2]
+          if (rgbSum < minSum) {
+            cloestX = i
+            minSum = rgbSum
+          }
+        }
+      }
+      resolve(cloestX * actualWidth / width)
+    }
+    img.onerror = reject
+    img.src = base64
+  })
+}
+
 async function login (user) {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: {
       width: 1024,
       height: 768
@@ -23,6 +52,38 @@ async function login (user) {
   await page.click('.login-tab.login-tab-r > a')
   user.username && await page.$eval('#loginname', (el, value) => el.value = value, user.username)
   user.password && await page.$eval('#nloginpwd', (el, value) => el.value = value, user.password)
+  await page.click('#loginsubmit')
+  await page.waitFor(1000)
+  // 需要验证码
+  let tryTimes = 0
+  while (await page.$('.JDJRV-bigimg')) {
+    console.log(`正在尝试破解验证码（第${++tryTimes}次）`)
+    const img = await page.$('.JDJRV-bigimg > img')
+    const distance = await getVerifyPosition(
+      await page.evaluate(element => element.getAttribute('src'), img),
+      await page.evaluate(element => parseInt(window.getComputedStyle(element).width), img)
+    )
+    const smallImg = await page.$('.JDJRV-smallimg')
+    const smallImgWidth = await page.evaluate(element => element.getBoundingClientRect().width, smallImg)
+    const dragBtn = await page.$('.JDJRV-slide-btn')
+    const dragBtnPosition = await page.evaluate(element => {
+      const {x, y, width, height} = element.getBoundingClientRect()
+      return {x, y, width, height}
+    }, dragBtn)
+    const x = dragBtnPosition.x + dragBtnPosition.width / 2
+    const y = dragBtnPosition.y + dragBtnPosition.height / 2
+    const distance1 = distance - 10 - smallImgWidth / 2
+    const distance2 = 10
+    await page.mouse.move(x, y)
+    await page.mouse.down()
+    await page.mouse.move(x + distance1, y, {steps: 30})
+    await page.waitFor(500)
+    await page.mouse.move(x + distance1 + distance2, y, {steps: 20})
+    await page.waitFor(500)
+    await page.mouse.up()
+    await page.waitFor(2000)
+  }
+
   // 等待用户登录成功，页面将跳转到 jd.com
   await page.waitForFunction('window.location.href.indexOf("https://www.jd.com") >= 0', {timeout: 0})
   const cookies = await page.cookies()
